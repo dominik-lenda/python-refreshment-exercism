c005f878df7cea2dd43264044b09951bf7f2dfa4
dominik.lenda <dlenda1729@gmail.com>
2021-07-29 00:57:00 +0200

    add solution approved by a mentor; but still I want to work on this examlpe

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,18 +1,11 @@
 class Matrix:
-    def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
-
+    def __init__(self, matrix_string: str):
+        split_lines = matrix_string.splitlines()
+        rows = [[int(j) for j in i.split()] for i in split_lines]
+        self.rows_converted = rows
 
     def row(self, index):
-        split_list = self.matrix_string.split("\n") if "\n" in self.matrix_string else self.matrix_string
-        split_nested_list = [i.split() for i in split_list]   
-        rows_integers = [[int(char) for char in chars] for chars in split_nested_list]
-        return rows_integers[index-1]
-
+        return self.rows_converted[index-1]
 
     def column(self, index):
-        split_list = self.matrix_string.split("\n") if "\n" in self.matrix_string else self.matrix_string
-        split_nested_list = [i.split() for i in split_list]   
-        rows_integers = [[int(char) for char in chars] for chars in split_nested_list]
-        columns = [row[index-1] for row in rows_integers]
-        return columns
+        return [row[index-1] for row in self.rows_converted]
